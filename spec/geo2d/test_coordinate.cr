149c4f663dfddbb1401615197c36101591f0a2a0
viachpaliy <viachpaliy@ukr.net>
2020-02-01 23:43:58 +0200

    rework Coordinate class

--- spec/geo2d/test_coordinate.cr
+++ spec/geo2d/test_coordinate.cr
@@ -79,6 +79,32 @@ describe Geo2D::Coordinate do
     c.y.should eq(5.678 - 2.34)
   end
 
+  it "must multiplicate coordinates" do
+    a = Geo2D::Coordinate.new 1.234, 5.678
+    b = Geo2D::Coordinate.new 2.34, 6.78
+    c = a * b
+    c.should be_a(Geo2D::Coordinate)  
+    c.x.should eq(1.234 * 2.34)
+    c.y.should eq(5.678 * 6.78)
+  end
+
+  it "must multiplicate Float64" do
+    a = Geo2D::Coordinate.new 1.234, 5.678
+    b = 2.34
+    c = a * b
+    c.should be_a(Geo2D::Coordinate)  
+    c.x.should eq(1.234 * 2.34)
+    c.y.should eq(5.678 * 2.34)
+  end
+
+  it "must divide Float64" do
+    a = Geo2D::Coordinate.new 1.234, 5.678
+    b = 2.34
+    c = a / b
+    c.should be_a(Geo2D::Coordinate)  
+    c.x.should eq(1.234 / 2.34)
+    c.y.should eq(5.678 / 2.34)
+  end
   
   it "must flip x and y of Coordinate" do
     origin_coord = Geo2D::Coordinate.new 1.234, 5.678
@@ -87,5 +113,75 @@ describe Geo2D::Coordinate do
     a.y.should eq(1.234)
     a.x.should eq(5.678)
   end 
+
+  it "test != method" do
+    a = Geo2D::Coordinate.new(1.0, 2.0)
+    b = Geo2D::Coordinate.new(1.0, 2.0)
+    c = Geo2D::Coordinate.new(2.0, 2.0)
+    result1 = a!= b
+    result2 = a!= c
+    result1.should be_false
+    result2.should be_true
+  end
+
+  it "test != method" do
+    a = Geo2D::Coordinate.new(1.0, 2.0)
+    b = Geo2D::Coordinate.new(1.0, 2.0)
+    c = Geo2D::Coordinate.new(2.0, 2.0)
+    result1 = a == b
+    result2 = a == c
+    result2.should be_false
+    result1.should be_true
+  end
+
+  it "test clone method" do
+    a = Geo2D::Coordinate.new(1.0, 2.0)
+    b = a.clone
+    b.x.should eq(1.0)
+    b.y.should eq(2.0)
+  end 
+
+  it "test magnitude method" do
+    a = Geo2D::Coordinate.new(3.0, 4.0)
+    a.magnitude.should eq(5.0)
+  end
+
+  it "test squared method" do
+    a = Geo2D::Coordinate.new(3.0, 4.0)
+    a.squared.should eq(25.0)
+  end
+
+  it "test dot method" do
+    a = Geo2D::Coordinate.new(1.5, 2.3)
+    b = Geo2D::Coordinate.new(0.0, 1.0)
+    a.dot(b).should eq(2.3)
+  end
+
+  describe "rotate method" do
+    it "rotate around (0.,0.) with a given angle vector" do
+      a = Geo2D::Coordinate.new(0.0, 1.0)
+      b = a.rotate Geo2D::Coordinate.new(0.6, 0.8)
+      b.x.should eq(-0.8)
+      b.y.should eq(0.6)
+    end
+    it "rotate around (0.,0.) with a given angle " do
+      a = Geo2D::Coordinate.new(0.0, 1.0)
+      b = a.rotate 0.927295218
+      b.x.should be_close(-0.8, 1e-6)
+      b.y.should be_close(0.6, 1e-6)
+    end
+    it "rotate around a point with a given angle vector" do
+      a = Geo2D::Coordinate.new(1.0, 2.0)
+      b = a.rotate Geo2D::Coordinate.new(1.0, 1.0), Geo2D::Coordinate.new(0.6, 0.8)
+      b.x.should be_close(0.2, 1e-6)
+      b.y.should eq(1.6)
+    end
+    it "rotate around a point with a given angle" do
+      a = Geo2D::Coordinate.new(1.0, 2.0)
+      b = a.rotate Geo2D::Coordinate.new(1.0, 1.0), 0.927295218
+      b.x.should be_close(0.2, 1e-6)
+      b.y.should be_close(1.6, 1e-6)
+    end
+  end
 
 end
--- src/geo2d/coordinate.cr
+++ src/geo2d/coordinate.cr
@@ -19,6 +19,10 @@ module Geo2D
       @y = Math.sin(angle)
     end
 
+   def clone
+      Coordinate.new x, y
+    end
+
     # Add two points to each other and return a new coordinate 
     def +(other : Coordinate)
       Coordinate.new x + other.x, y + other.y
@@ -39,10 +43,72 @@ module Geo2D
       Coordinate.new x - offset, y - offset
     end
 
+    # Multiplication two points and return a new coordinate
+    def *(other : Coordinate)
+      Coordinate.new x * other.x, y * other.y
+    end 
+
+    # Multiplication by Float64
+    def *(s : Float64)
+      Coordinate.new x * s, y * s
+    end
+
+    # Division by Float64
+    def /(s : Float64)
+      Coordinate.new x / s, y / s
+    end
+
+    
+    def !=(coord : Coordinate) : Bool
+      x != coord.x || y != coord.y 
+    end
+
+    # Checks for the equality of Coordinate
+    def ==(coord : Coordinate) : Bool
+      x == coord.x && y == coord.y 
+    end
+
+    # Magnitude or length of the point relative to zero
+    def magnitude
+      Math.sqrt(x * x + y * y)
+    end
+
+    # Squared of this Coordinate : return x * x + y * y
+    def squared
+      x * x + y * y
+    end
+
+    # Dot product with other coordinate : return _x * coord._x + _y * coord._y
+    def dot(coord : Coordinate)
+      x * coord.x + y * coord.y
+    end
+
     # Flips the x and y of Coordinate
     def flip_xy
       Coordinate.new y, x
     end
+
+    # Rotate around (0.,0.) with a given angle vector
+    def rotate(angle_vector : Coordinate)
+      x0 = x * angle_vector.x - y * angle_vector.y
+      y0 = x * angle_vector.y + y * angle_vector.x
+      Coordinate.new x0, y0
+    end
+
+    # Rotate around (0.,0.) with a given angle
+    def rotate(angle : Float64)
+      rotate(Coordinate.new(angle))
+    end
+
+    # Rotate around a point with a given angle vector
+    def rotate(point : Coordinate, angle_vector : Coordinate)
+      point + (self - point).rotate(angle_vector)
+    end
+
+    # Rotate around a point with a given angle 
+    def rotate(point : Coordinate, angle : Float64)
+      rotate(point, Coordinate.new(angle))
+    end 
 
   end 
 
